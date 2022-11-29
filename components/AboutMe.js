7683e716c2dd9f2ee831b0c02768f1ec66c24419
Gaye Y <96841321+gaiaysl@users.noreply.github.com>
2022-11-29 21:56:25 +0300

    designer-fixed

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -5,10 +5,12 @@ export default function AboutMe(){
       <div  className=" h-72 w-96 mr-48 ">
         <h1 className="mb-4 font text-4xl font-bold tracking-tight text-white sm:text-6xl">About <span className='text-cyan-500'>me</span></h1>
        <div className="break-words overflow-hidden ">
-        <p >rthrhtr
-        hrhtrhsrhrt
-        jtdyjhyjdyjyt
-        jlyılkıyLorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
+        <p >
+          deneme yazısıdır. deneme yazısıdır deneme yazısıdır deneme yazısıdır
+          deneme yazısıdır deneme yazısıdır deneme yazısıdır deneme yazısıdır
+          deneme yazısıdır
+          deneme yazısıdır
+          deneme yazısıdır
      
         </p>
         </div>
