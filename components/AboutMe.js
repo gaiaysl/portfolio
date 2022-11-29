6c99e909aa100967163d5ee34f8d0939c66069a2
Gaye Y <96841321+gaiaysl@users.noreply.github.com>
2022-11-29 22:11:56 +0300

    designer-fixed

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,7 +1,7 @@
 
 export default function AboutMe(){
   return(
-    <div id ='about' className="mx-auto flex mb-40 max-w-7xl px-4 sm:px-6 lg:px-8" >
+    <div id='aboutme' className="mx-auto flex mb-40 max-w-7xl px-4 sm:px-6 lg:px-8" >
       <div  className=" h-72 w-96 mr-48 ">
         <h1 className="mb-4 font text-4xl font-bold tracking-tight text-white sm:text-6xl">About <span className='text-cyan-500'>me</span></h1>
        <div className="break-words overflow-hidden ">
--- components/Designer.js
+++ components/Designer.js
@@ -4,20 +4,20 @@
     return(
       <div id='home' className="mt-48 mb-48 mx-auto flex flex-row justify-evenly  " >
         
-        <div className="mt-24">
+        <div className="mt-24  ">
           <div className=" ml-24">
           <h1 className="font text-6xl font-bold tracking-tight text-white sm:text-6xl">CREATIVE UI </h1>
           
           <span className='  absolute sm:text-6xl font text-8xl font-bold tracking-tight text-cyan-500'>DESIGNER</span>
           </div>
-          <div className="-mt-4">
+          <div className="-mt-4 ">
           <img src='/Images/ok.png'  />
           </div>
         <div className=" -mt-12 ml-24 ">
           <button class=" mr-4 bg-cyan-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
   Hire me
 </button>
-<button class="bg-gray-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+<button class="bg-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
   Download CV
 </button> </div>
 
