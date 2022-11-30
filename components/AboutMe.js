9d98e7b4443d3177398ab4a1c71f592ebf39bbe2
Gaye Y <96841321+gaiaysl@users.noreply.github.com>
2022-11-30 21:03:20 +0300

    title

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -3,7 +3,7 @@ export default function AboutMe(){
   return(
     <div id='aboutme' className=" text-white sm:mx-auto flex sm:mb-40 mb-20 sm:max-w-7xl sm:px-4 sm:px-6 sm:flex-row lg:px-8 flex-col ml-10  " >
       <div  className=" h-72 w-72  mr-48 sm:w-96 ">
-        <h1 className="mb-4 font text-4xl text-center font-bold tracking-tight text-white sm:text-6xl">About <span className='text-cyan-500'>me</span></h1>
+        <h1 className="mb-4 font text-4xl  font-bold tracking-tight text-white sm:text-6xl">About <span className='text-cyan-500'>me</span></h1>
        <div className="break-words overflow-hidden h-48 ">
         <p >
         I have 2 years of experience in frontend developer field.
@@ -12,7 +12,7 @@ I am a student at Istanbul University Computer Programming Department. The proje
         <a href='https://github.com/gaiaysl' className="underline hover:text-cyan-500"> my github account</a>
         </div>
       </div>
-      <div className="  mr-8">
+      <div className="  mr-6">
         <img src='/Images/man-background.png' className="h-full  object-cover object-center"/>
       </div>
       
